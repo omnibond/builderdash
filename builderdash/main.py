24884d9590e3830ae91503d7bae504762214e5ad
Mary Brandenburg <mary@omnibond.com>
2023-10-31 08:58:54 -0400

    use image family regardless of build type

--- builderdash/main.py
+++ builderdash/main.py
@@ -302,8 +302,7 @@ def googleInstance(myBuild):
     machine_type = "zones/%s/machineTypes/%s" % (zone, str(myBuild.instancetype))
 
     # use image family if applicable
-    if (myBuild.buildtype == "base" or myBuild.buildtype == "kernel") and \
-        hasattr(myBuild, "imagefamily") and myBuild.imagefamily != "none":
+    if hasattr(myBuild, "imagefamily") and myBuild.imagefamily != "none":
         try:
             # Name of the image family to search for.
             family = myBuild.imagefamily
